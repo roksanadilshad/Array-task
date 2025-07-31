a981dea17fe2edd8802c2e3abc87376cc79367a3
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-31 20:27:16 +0600

    Add membership.js

---
+++ membership.js
@@ -0,0 +1,9 @@
+let books = ['opekkha', 'hazar-bochor-dhore', 'letters-to-millena', 'lilaboti']
+
+console.log(books.includes('lilaboti'));
+   if(books.includes('letters-to-millena')){
+    console.log("It's a sad story..")
+   }
+   else{
+    console.log("I have no idea about this book");
+   }
