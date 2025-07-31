4a0d45e7f24be97287d08a89ee48a86b3db6de7f
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-31 20:18:25 +0600

    add-remove

---
+++ add-remove.js
@@ -0,0 +1,9 @@
+let spot = ['sajek','garopahar', 'sitakundo'];
+
+console.log(spot);
+spot.push('coxs-bazar');
+console.log(spot);
+spot.push('kuakata', 'bagha');
+console.log(spot);
+spot.pop();
+console.log(spot);
