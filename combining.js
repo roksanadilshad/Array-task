39249b91fa002edaca2e88a3618fe892f729a158
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-07-31 20:44:11 +0600

    Add combind.js

---
+++ combining.js
@@ -0,0 +1,9 @@
+const girls = ['rodela', 'sabona', 'shinha', 'priya'];
+const hobby = ['painting', 'braking-ruls', 'talking', 'singing'];
+ 
+const combindArray = girls.concat(hobby);
+
+console.log(combindArray);
+console.log(girls);
+console.log(hobby);
+
